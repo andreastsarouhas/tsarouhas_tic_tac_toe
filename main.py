40c70ee5a4aa46d3b3e73652b4bfab3b0f252307
andreastsarouhas <132574984+andreastsarouhas@users.noreply.github.com>
2023-07-27 00:35:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-player_1 = "X"
-player_2 = "O"
-
 box_1 = " "
 box_2 = " "
 box_3 = " "
@@ -19,34 +16,43 @@ def print_board():
     print(f"|{boxes[6]}|{boxes[7]}|{boxes[8]}|")
 
 
-while True:    
+while True: 
 
-    print_board()
+        print_board()
 
-    position = int(input("Select position: "))
+        position = int(input("Select position: ")) - 1
+        
+        if not position <= 8 and position >= 0:
+            print("Position out of range")
+            print_board()
+            position = int(input("Select position: ")) - 1
 
-    position -= 1
+        boxes[position] = input("Fill with X or O: ")
 
-    boxes[position] = input("Fill with X or O: ")
+        if not boxes[position] == "X" or boxes[position] == "O":
+            print("Invalid input") 
+            boxes[position] = input("Fill with X or O: ")
 
-    if (boxes[0] == player_1 and boxes[1] == player_1 and boxes[2] == player_1) \
-            or (boxes[0] == player_1 and boxes[3] == player_1 and boxes[6] == player_1) \
-            or (boxes[3] == player_1 and boxes[4] == player_1 and boxes[5] == player_1) \
-            or (boxes[1] == player_1 and boxes[4] == player_1 and boxes[7] == player_1) \
-            or (boxes[0] == player_1 and boxes[4] == player_1 and boxes[8] == player_1) \
-            or (boxes[2] == player_1 and boxes[5] == player_1 and boxes[8] == player_1) \
-            or (boxes[6] == player_1 and boxes[7] == player_1 and boxes[8] == player_1) \
-            or (boxes[2] == player_1 and boxes[4] == player_1 and boxes[6] == player_1):
-        print("Player 1 wins")
-        break
-    elif (boxes[0] == player_2 and boxes[1] == player_2 and boxes[2] == player_2) \
-            or (boxes[0] == player_2 and boxes[3] == player_2 and boxes[6] == player_2) \
-            or (boxes[3] == player_2 and boxes[4] == player_2 and boxes[5] == player_2) \
-            or (boxes[1] == player_2 and boxes[4] == player_2 and boxes[7] == player_2) \
-            or (boxes[0] == player_2 and boxes[4] == player_2 and boxes[8] == player_2) \
-            or (boxes[2] == player_2 and boxes[5] == player_2 and boxes[8] == player_2) \
-            or (boxes[6] == player_2 and boxes[7] == player_2 and boxes[8] == player_2) \
-            or (boxes[2] == player_2 and boxes[4] == player_2 and boxes[6] == player_2):
-        print("Player 2 wins")
-        break
+        if (boxes[0] == "X" and boxes[1] == "X" and boxes[2] == "X") \
+            or (boxes[0] == "X" and boxes[3] == "X" and boxes[6] == "X") \
+            or (boxes[3] == "X" and boxes[4] == "X" and boxes[5] == "X") \
+            or (boxes[1] == "X" and boxes[4] == "X" and boxes[7] == "X") \
+            or (boxes[0] == "X" and boxes[4] == "X" and boxes[8] == "X") \
+            or (boxes[2] == "X" and boxes[5] == "X" and boxes[8] == "X") \
+            or (boxes[6] == "X" and boxes[7] == "X" and boxes[8] == "X") \
+            or (boxes[2] == "X" and boxes[4] == "X" and boxes[6] == "X"):
+            print_board()
+            print("Player 1 wins")
+            break
+        if (boxes[0] == "O" and boxes[1] == "O" and boxes[2] == "O") \
+            or (boxes[0] == "O" and boxes[3] == "O" and boxes[6] == "O") \
+            or (boxes[3] == "O" and boxes[4] == "O" and boxes[5] == "O") \
+            or (boxes[1] == "O" and boxes[4] == "O" and boxes[7] == "O") \
+            or (boxes[0] == "O" and boxes[4] == "O" and boxes[8] == "O") \
+            or (boxes[2] == "O" and boxes[5] == "O" and boxes[8] == "O") \
+            or (boxes[6] == "O" and boxes[7] == "O" and boxes[8] == "O") \
+            or (boxes[2] == "O" and boxes[4] == "O" and boxes[6] == "O"):
+            print_board()
+            print("Player 2 wins")
+            break
         
